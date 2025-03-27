1ea09f8e19a990f7adc02aafcb1912e5ef846de2
Humiditii <humiditii45@gmail.com>
2025-03-27 17:55:44 +0100

    fix(mvp): open a default endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './common/guard/decorator/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public(true)
   @Get()
   getHello(): string {
     return this.appService.getHello();
